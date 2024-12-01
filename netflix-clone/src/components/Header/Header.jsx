db8f26bf50c6d8d5c636e30edf3942885ac40a60
tekle.negash@mu.edu.et <TekEva>
2024-12-01 12:40:14 +0300

    gh-pages and scripts modified

--- netflix-clone/src/components/Header/Header.jsx
+++ netflix-clone/src/components/Header/Header.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+// Importing required dependencies and assets
+import React from "react";
+import Netflixlogo from "../../assets/images/NetflixLogo.png";
+import "./header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
+// Functional component for the Header section
 const Header = () => {
   return (
-    <div>my header</div>
-  )
-}
+    <>
+      {/* Header container */}
+      <div className="header-outer-container">
+        <div className="header-container">
+          {/* Left side of the header containing navigation links */}
+          <div className="left-header">
+            <ul>
+              <li className="logo">
+                {/* Netflix logo */}
+                <img src={Netflixlogo} width="100" />
+              </li>
+              {/* Navigation menu */}
+              <li>Home</li>
+              <li>TVShows</li>
+              <li>Movies</li>
+              <li>Latest</li>
+              <li>MyList</li>
+              <li>Brwose by Language</li>
+            </ul>
+          </div>
+          {/* Right side of the header containing user-related icons */}
+          <div className="right-header">
+            <ul>
+              <li>
+                {/* Search icon */}
+                <SearchIcon />{" "}
+              </li>
+              <li>
+                {/* Notifications icon */}
+                <NotificationsNoneIcon />
+              </li>
+              <li>
+                {/* Account icon */}
+                <AccountBoxIcon />
+              </li>
+              <li>
+                {/* Dropdown icon */}
+                <ArrowDropDownIcon />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Header
+// Exporting Header component
+export default Header;
--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
-import Header from '../../components/Header/Header'
-import Footer from '../../components/Footer/Footer'
+// Importing required components
+import React from "react";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import Banner from "../../components/Banner/Banner";
+import RowList from "../../components/Rows/RowList/RowList";
 
+// Functional component for the Home page
 const Home = () => {
   return (
     <div>
-        <Header/>
-        <Footer/>
-
+      {/* Header section */}
+      <Header />
+      {/* Banner section */}
+      <Banner />
+      {/* Row list section for displaying categorized movies */}
+      <RowList />
+      {/* Footer section */}
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+// Exporting Home component
+export default Home;
